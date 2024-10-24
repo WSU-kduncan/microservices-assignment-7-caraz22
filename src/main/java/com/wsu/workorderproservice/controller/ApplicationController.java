001574e899714d1b92e164cf475bfc346280387c
caraz22 <cszoke22@gmail.com>
2024-10-24 15:18:06 -0400

    in class work

--- src/main/java/com/wsu/workorderproservice/controller/ApplicationController.java
+++ src/main/java/com/wsu/workorderproservice/controller/ApplicationController.java
@@ -1,5 +1,37 @@
 package com.wsu.workorderproservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wsu.workorderproservice.dto.ServiceResponseDTO;
+import com.wsu.workorderproservice.service.ApplicationService;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+
+import static com.wsu.workorderproservice.utilities.Constants.MESSAGE;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
 public class ApplicationController {
     
+    private final ApplicationService applicationService;
+
+    @GetMapping(value = "{application_id}")
+    public ResponseEntity<ServiceResponseDTO> getApplication(@PathVariable Integer application_id) {
+        return new ResponseEntity<>(ServiceResponseDTO.builder().meta(Map.of(MESSAGE, "Appliation retrieved successfully")).data(applicationService.get(application_id)).build(). HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<ServiceResponseDTO> addApplication(@RequestBody Application application) {
+        return new ResponseEntity<>(ServiceResponseDTO.builder()meta(Map.of(MESSAGE, "Application added successfully")).data(applicationService.add(application)).build(), HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/wsu/workorderproservice/service/ApplicationService.java
+++ src/main/java/com/wsu/workorderproservice/service/ApplicationService.java
@@ -1,5 +1,26 @@
 package com.wsu.workorderproservice.service;
 
+import com.wsu.workorderproservice.exception.InvalidRequestException;
+import com.wsu.workorderproservice.exception.DatabaseErrorException;
+import com.wsu.workorderproservice.model.Application;
+import com.wsu.workorderproservice.repository.ApplicationRepository;
+
+import java.util.Optional;
+
 public class ApplicationService {
     
+    private final ApplicationRepository applicationRepository;
+
+    public Application get(Integer application_id) {
+        Optional<Application> application = applicationRepository.findById(application_id);
+        if (application.isEmpty()) {
+            throw new InvalidRequestException("Invalid application ID");
+        } 
+        try {
+            return applicationRepository.findById(application_id).orElse(null);
+        } catch (Exception e) {
+            log.error("Failed to retrieve application details. application_id:{}, Exception:{}", application_id, e);
+            throw new DatabaseErrorException("Failed to retrieve application details.", e);
+        }
+    }
 }
