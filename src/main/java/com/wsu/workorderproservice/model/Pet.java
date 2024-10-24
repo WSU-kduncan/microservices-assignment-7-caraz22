8df1e782cabdc454d26ba4463a890fe79e1cdf46
caraz22 <cszoke22@gmail.com>
2024-10-24 13:30:43 -0400

    created all folders and files

---
+++ src/main/java/com/wsu/workorderproservice/model/Pet.java
@@ -0,0 +1,20 @@
+package com.wsu.workorderproservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "Pets")
+public class Pet {
+    
+}
---
+++ src/main/java/com/wsu/workorderproservice/service/StaffService.java
@@ -0,0 +1,5 @@
+package com.wsu.workorderproservice.service;
+
+public class StaffService {
+    
+}
