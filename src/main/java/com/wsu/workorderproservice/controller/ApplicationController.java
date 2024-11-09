d028e90386deb97dc026a3ff9e7bc566319ed6cb
caraz22 <cszoke22@gmail.com>
2024-11-09 12:53:40 -0500

    done what i can do for today

--- src/main/java/com/wsu/workorderproservice/controller/ApplicationController.java
+++ src/main/java/com/wsu/workorderproservice/controller/ApplicationController.java
@@ -5,11 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wsu.workorderproservice.dto.ServiceResponseDTO;
+import com.wsu.workorderproservice.model.Application;
 import com.wsu.workorderproservice.service.ApplicationService;
 
 import lombok.RequiredArgsConstructor;
@@ -27,11 +29,16 @@ public class ApplicationController {
 
     @GetMapping(value = "{application_id}")
     public ResponseEntity<ServiceResponseDTO> getApplication(@PathVariable Integer application_id) {
-        return new ResponseEntity<>(ServiceResponseDTO.builder().meta(Map.of(MESSAGE, "Appliation retrieved successfully")).data(applicationService.get(application_id)).build(). HttpStatus.OK);
+        return new ResponseEntity<>(ServiceResponseDTO.builder().meta(Map.of(MESSAGE, "Application retrieved successfully")).data(applicationService.get(application_id)).build(). HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<ServiceResponseDTO> addApplication(@RequestBody Application application) {
         return new ResponseEntity<>(ServiceResponseDTO.builder()meta(Map.of(MESSAGE, "Application added successfully")).data(applicationService.add(application)).build(), HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "{application_id}")
+    public ResponseEntity<ServiceResponseDTO> updateApplication(@PathVariable Integer application_id, @RequestBody Application application) {
+        return new ResponseEntity<>(ServiceResponseDTO.builder().meta(Map.of(MESSAGE, "Application updated successfully")).data(applicationService.update(application_id, application)).build(), HttpStatus.OK);
     }
 }
--- src/main/java/com/wsu/workorderproservice/service/ApplicationService.java
+++ src/main/java/com/wsu/workorderproservice/service/ApplicationService.java
@@ -5,6 +5,8 @@ import com.wsu.workorderproservice.exception.DatabaseErrorException;
 import com.wsu.workorderproservice.model.Application;
 import com.wsu.workorderproservice.repository.ApplicationRepository;
 
+import jakarta.transaction.Transactional;
+
 import java.util.Optional;
 
 public class ApplicationService {
@@ -21,6 +23,31 @@ public class ApplicationService {
         } catch (Exception e) {
             log.error("Failed to retrieve application details. application_id:{}, Exception:{}", application_id, e);
             throw new DatabaseErrorException("Failed to retrieve application details.", e);
+        }
+    }
+
+    @Transactional(rollbackOn = Exception.class)
+    public Application add(Application application) {
+        try {
+            // stuff???
+        } catch (Exception e) {
+            log.error("Failed to add application. Exception: ", e);
+            throw new DatabaseErrorException("Failed to add application", e);
+        }
+    }
+
+    @Transactional(rollbackOn = Exception.class) 
+    public Application update(Integer application_id, Application application) {
+        Optional<Application> applicationResp = applicationRepository.findById(application_id);
+        if (applicationResp.isEmpty()) {
+            throw new InvalidRequestException("Invalid application ID");
+        }
+        
+        try {
+            // stuff
+        } catch (Exception e) {
+            log.error("Failed to update application. application_id:{}, Exception:{}", application_id, e);
+            throw new DatabaseErrorException("Failed to update application.", e);
         }
     }
 }
